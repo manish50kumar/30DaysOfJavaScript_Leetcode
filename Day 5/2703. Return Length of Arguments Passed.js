f746a08a6948835e34bff1d0f01f85409f2cdbd1
manish50kumar <manish5060773@gmail.com>
2023-08-29 19:42:29 +0530

    2703. Return Length of Arguments Passed

---
+++ Day 5/2703. Return Length of Arguments Passed.js
@@ -0,0 +1,15 @@
+
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+
+// 2703. Return Length of Arguments Passed
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
